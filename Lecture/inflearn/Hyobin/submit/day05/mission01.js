e8f535a81f62d53fe6b3deee4d85889adbe1e2d0
LimdaeIl <optional94@naver.com>
2025-07-01 14:42:31 +0900

    submit day 05

--- Lecture/inflearn/Hyobin/submit/day05/mission01.js
+++ Lecture/inflearn/Hyobin/submit/day05/mission01.js
@@ -2,7 +2,10 @@ console.log(sumAndDouble(1, 2, 3, 4));
 
 function sumAndDouble(...rest) {
     const sum = rest.reduce((acc, cur) => acc + cur, 0);
-    const multi = rest.map((x) => x * x);
+    const multi = rest.map((x) => x * 2);
 
-    return {sum, multi};
+    return {
+        "sum": sum,
+        "multi": multi
+    };
 }
