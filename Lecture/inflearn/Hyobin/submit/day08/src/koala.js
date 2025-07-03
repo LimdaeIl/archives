f4d5ac1a182a316a680559245168e99843f7ca0a
LimdaeIl <optional94@naver.com>
2025-07-03 14:01:37 +0900

    day 8+

--- Lecture/inflearn/Hyobin/submit/day08/src/koala.js
+++ Lecture/inflearn/Hyobin/submit/day08/src/koala.js
@@ -3,8 +3,8 @@ const API_URL = 'https://animal-api-two.vercel.app/';
 const $content = document.querySelector('div.content');
 let template = [];
 
-const getData = async () => {
-    let res = await fetch(API_URL);
+const getData = async (name) => {
+    let res = await fetch(`${API_URL}${name}`);
     try {
         if (res) {
             let data = await res.json();
@@ -19,4 +19,4 @@ const getData = async () => {
     }
 }
 
-getData();
+getData('koala');
