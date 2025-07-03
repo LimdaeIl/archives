201026d075c3b6710f8c2eb434c14435c6af704a
LimdaeIl <optional94@naver.com>
2025-07-03 14:55:57 +0900

    animal2

---
+++ Lecture/inflearn/Hyobin/animal2/src/App.js
@@ -0,0 +1,10 @@
+import TabBar from "./components/TabBar.js";
+import Content from "./components/Content.js";
+
+// 컴포넌트 단위로 모듈화
+export default function App($app) {
+
+    const tabBar = new TabBar();
+    const content = new Content();
+
+}
